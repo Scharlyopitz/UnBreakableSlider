184fcb40f4395f84702fced73ea2d158723b839c
Scharly <scharly.opitz@gmail.com>
2025-07-18 11:33:33 +0200

    Mise en page

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,5 +1,27 @@
-import React from "react";
-
 export default function Slider() {
-  return <div>Slider</div>;
+  const slides = [
+    { number: "001", text: "Hey Yo" },
+    { number: "002", text: "Nirvana" },
+    { number: "003", text: "Shakira" },
+    { number: "004", text: "Drake" },
+    { number: "005", text: "Niki Minaj" },
+    { number: "006", text: "Beyonce" },
+  ];
+
+  return (
+    <section id="Slider">
+      {slides.map(({ number, text }, i) => {
+        return (
+          <div key={i} className="slide">
+            <div className="numberContainer">
+              <p>{number}</p>
+            </div>
+            <div className="textContainer">
+              <p>{text}</p>
+            </div>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
